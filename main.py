0b9cb890976d30ba36b5c659fa31c4642cc51b98
Faith Smith <Selmavilleeverafter@gmail.com>
2026-02-06 19:21:47 -0600

    Added division function

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-num1 = float(input("Enter first number: "))
+number1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
 userOperator = (input("Please enter the operation you with to do: (+, -, *, /)"))
 
 
+
+
+elif userOperator == "/":
+    if num2 == 0:
+        print("Error: Division by zero")
+    else:
+        print(float(num1) / float(num2))
