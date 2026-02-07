791602c3611423b1c4108500379d4e4f26819953
Jacob <jakegrandone@gmail.com>
2026-02-06 19:22:01 -0600

    Added addition and subtraction output

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
 userOperator = (input("Please enter the operation you with to do: (+, -, *, /)"))
-
+if(userOperator == "+"):
+    print(num1 + num2)
+elif(userOperator == "-"):
+    print(num1 - num2)
 
