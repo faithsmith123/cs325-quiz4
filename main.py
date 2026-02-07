bfeb8c9bdc40152035cf9c51d5e1e23314325630
Faith Smith <Selmavilleeverafter@gmail.com>
2026-02-06 20:19:39 -0600

    changed line to create merge conflict

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 number1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
 userOperator = (input("Please enter the operation you with to do: (+, -, *, /)"))
-
+if (userOperator = "x")
 
 
 
